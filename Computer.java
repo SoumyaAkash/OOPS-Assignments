b85511adf6bc1efaa0ff49d53e9b0a6871917c77
SoumyaAkash <107401356+SoumyaAkash@users.noreply.github.com>
2022-06-25 17:41:15 +0100

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,17 @@
+package system;
+
+public class Computer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Computer C =new Computer();
+		C.computerModel();
+	}
+public static void computerModel()
+{
+	String Cmodel="Apple iMac 21.5";
+	System.out.println("Computer model is " +Cmodel);
+}
+	}
+
+
