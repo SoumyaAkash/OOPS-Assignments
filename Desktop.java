522b025b0659212570a83d5d9035dd3c32891df0
SoumyaAkash <107401356+SoumyaAkash@users.noreply.github.com>
2022-06-25 17:41:38 +0100

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,19 @@
+package system;
+
+public class Desktop extends Computer{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop D = new Desktop();
+		computerModel();
+		D.desktopSize();
+	}
+public void desktopSize()
+	{
+		
+	double Dsize =21.5;
+System.out.println("Desktop size is "+Dsize);
+	}
+
+}
+
