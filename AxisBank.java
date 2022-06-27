a98ec868b662583a0981b6942a000d02c7d3c4b3
SoumyaAkash <107401356+SoumyaAkash@users.noreply.github.com>
2022-06-27 11:08:08 +0100

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class AxisBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub (Method overloading)
+AxisBank A=new AxisBank();
+A.deposit();
+	}
+	public void deposit()
+	{
+		int deposit=500000;
+		System.out.println("Deposit amount in Axis bank is "+deposit);
+	}
+	
+}
